c58efb768115b1bc28adb2ed0e1ed76bca38553d
Alyona <olenapav97@gmail.com>
2025-01-25 11:11:27 +0200

    fin

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,23 +1,16 @@
+import { useSelector } from "react-redux";
+import { selectFilteredContacts } from "../../redux/contactsSlice";
 import Contact from "../Contact/Contact";
 import s from "./ContactList.module.css";
 
-import { useSelector } from "react-redux";
-import { selectContacts } from "../../redux/contactsSlice";
-import { selectNameFilter } from "../../redux/filtersSlice";
-
 const ContactList = () => {
-  const contacts = useSelector(selectContacts);
-  const valueOfFilter = useSelector(selectNameFilter);
+  const filteredContacts = useSelector(selectFilteredContacts);
 
   return (
     <ul className={s.contactList}>
-      {contacts
-        .filter((contact) =>
-          contact.name.toLowerCase().includes(valueOfFilter.toLowerCase())
-        )
-        .map(({ id, number, name }) => (
-          <Contact key={id} id={id} number={number} name={name} />
-        ))}
+      {filteredContacts.map(({ id, number, name }) => (
+        <Contact key={id} id={id} number={number} name={name} />
+      ))}
     </ul>
   );
 };
--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,6 +1,5 @@
 import { createSlice, createSelector } from "@reduxjs/toolkit";
 import { fetchContacts, addContact, deleteContact } from "./contactsOps";
-
 import { selectNameFilter } from "./filtersSlice";
 
 const handlePending = (state) => {
@@ -10,6 +9,11 @@ const handlePending = (state) => {
 const handleRejected = (state, action) => {
   state.loading = false;
   state.error = action.payload;
+};
+
+const handleFulfilled = (state) => {
+  state.error = null;
+  state.loading = false;
 };
 
 const contactsSlice = createSlice({
@@ -23,24 +27,21 @@ const contactsSlice = createSlice({
     builder
       .addCase(fetchContacts.pending, handlePending)
       .addCase(fetchContacts.fulfilled, (state, action) => {
-        state.error = null;
-        state.loading = false;
+        handleFulfilled(state);
         state.items = action.payload;
       })
       .addCase(fetchContacts.rejected, handleRejected)
 
       .addCase(addContact.pending, handlePending)
       .addCase(addContact.fulfilled, (state, action) => {
-        state.error = null;
-        state.loading = false;
+        handleFulfilled(state);
         state.items.push(action.payload);
       })
       .addCase(addContact.rejected, handleRejected)
 
       .addCase(deleteContact.pending, handlePending)
       .addCase(deleteContact.fulfilled, (state, action) => {
-        state.error = null;
-        state.loading = false;
+        handleFulfilled(state);
         state.items = state.items.filter(
           (item) => item.id !== action.payload.id
         );
@@ -52,15 +53,15 @@ const contactsSlice = createSlice({
 export const contactsReducer = contactsSlice.reducer;
 
 export const selectContacts = (state) => state.contacts.items;
-
-export const selectIsLoading = (state) => state.contacts.isLoading;
-
+export const selectIsLoading = (state) => state.contacts.loading;
 export const selectError = (state) => state.contacts.error;
 
 export const selectFilteredContacts = createSelector(
   [selectContacts, selectNameFilter],
-  (contacts, nameFilter) =>
-    contacts?.filter((contact) =>
-      contact.name.toLowerCase().includes(nameFilter?.toLowerCase())
-    )
+  (contacts, nameFilter) => {
+    const normalizedFilter = nameFilter?.toLowerCase() || "";
+    return contacts.filter((contact) =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  }
 );
